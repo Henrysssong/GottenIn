dca4f8461bdcbb57041159a7c5d69a9d100a9422
Henry Song <feidong2002@berkeley.edu>
2023-08-31 14:35:47 -0700

    employee portal MVP fixed again

--- src/Employee.js
+++ src/Employee.js
@@ -7,14 +7,13 @@ function Employee() {
             <h2>Find applicant you think is gonna make it and contact them!</h2>
             
             <iframe 
+                title="Applicant Spreadsheet"
                 src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT7gGPst2NLdFeEpLajPJrMMvPEly5bN9hPyer6fgVypiHYUipODbZijTUEUYkN7LDb0YF-T8BHUrpj/pubhtml?widget=true&amp;headers=false"
                 width="100%" 
                 height="500" 
                 frameborder="0" 
                 scrolling="no">
             </iframe>
-            
-            {/* Other content */}
         </div>
     );
 }
