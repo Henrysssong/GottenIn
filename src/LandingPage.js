d4257c3a05d833906e246b8d528798ab275e2779
Henry Song <feidong2002@berkeley.edu>
2023-08-31 09:36:24 -0700

    3

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -9,14 +9,14 @@ function LandingPage() {
       
       <div className="portal-container">
         <div className="portal-block">
-          <img src="path_to_applicant_image.jpg" alt="Applicant" />
-          <p>Description for applicants</p>
+          <img src="applicant_image.jpg" alt="Applicant" />
+          <p>Nowadays, referral had gotten more and more crucial in the recruiting process. According to one of Amazon's hiring manager, 11/13 of offers are for those who had a referral. Yet it is slow and tiring to keep asking for referral and getting ignored. At GottenIn, we connect you with employees from your dream company so and save your precious time.</p>
           <button onClick={() => window.location.href='/applicant'}>Applicant Portal</button>
         </div>
         
         <div className="portal-block">
-          <img src="path_to_employee_image.jpg" alt="Employee" />
-          <p>Description for employees</p>
+          <img src="employee_image.jpg" alt="Employee" />
+          <p>Getting a bonus or helping others is cool. But come on, job already takes a lot of time. You don't want to waste your time and spend your mind talking to applicants you don't feel qualified. At GottenIn, you have access to a sheet of applicants. Just contact or refer the ones who you think have a shot.</p>
           <button onClick={() => window.location.href='/employee'}>Employee Portal</button>
         </div>
       </div>
