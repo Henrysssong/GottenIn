340018d0b78201c49d1eb1269132e1c55b54f7b2
Henry Song <feidong2002@berkeley.edu>
2023-08-31 10:18:30 -0700

    employee portal MVP fixed

--- src/Employee.js
+++ src/Employee.js
@@ -4,7 +4,7 @@ import './Employee.css';
 function Employee() {
     return (
         <div>
-            <h2>Find applicant you think is gonna make it and contact them!</h2>}
+            <h2>Find applicant you think is gonna make it and contact them!</h2>
             
             <iframe 
                 src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT7gGPst2NLdFeEpLajPJrMMvPEly5bN9hPyer6fgVypiHYUipODbZijTUEUYkN7LDb0YF-T8BHUrpj/pubhtml?widget=true&amp;headers=false"
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -8,13 +8,13 @@ function LandingPage() {
       
       <div className="portal-container">
         <div className="portal-block">
-          <img src="applicant_image.jpg" alt="Applicant" />
+          <img src="/applicant_image.jpg" alt="Applicant" />
           <p className="landingPageText">Nowadays, referral had gotten more and more crucial in the recruiting process. According to one of Amazon's hiring manager, 11/13 of offers are for those who had a referral. Yet it is slow and tiring to keep asking for referral and getting ignored. At GottenIn, we connect you with employees from your dream company so and save your precious time.</p>
           <button onClick={() => window.location.href='/applicant'}>Applicant Portal</button>
         </div>
         
         <div className="portal-block">
-          <img src="employee_image.jpg" alt="Employee" />
+          <img src="/employee_image.jpg" alt="Employee" />
           <p className="landingPageText">Getting a bonus or helping others is cool. But come on, job already takes a lot of time. You don't want to waste your time and spend your mind talking to applicants you don't feel qualified. At GottenIn, you have access to a sheet of applicants. Just contact or refer the ones who you think have a shot.</p>
           <button onClick={() => window.location.href='/employee'}>Employee Portal</button>
         </div>
